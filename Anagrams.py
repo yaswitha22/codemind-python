4e1487aaa0c87caf1eb7efbaecfd0f11d3bd24e8
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-07-13 12:12:48 +0530

    s1=input() s2=input() s1=sorted(s1.lower()) s2=sorted(s2.lower()) if s1==s2:     print("True") else:     print("False")

--- Anagrams.py
+++ Anagrams.py
@@ -1,13 +1,8 @@
 s1=input()
 s2=input()
-c=0
-s1=s1.lower()
-s2=s2.lower()
-for i in s1:
-    for j in s2:
-        if i==j:
-            c+=1
-if c==len(s1) and c==len(s2):
+s1=sorted(s1.lower())
+s2=sorted(s2.lower())
+if s1==s2:
     print("True")
 else:
     print("False")
