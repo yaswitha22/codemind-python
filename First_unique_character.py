6b6d8b703f34fb2ec8ef3962e4dd6ae94caad0e6
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-07-20 10:11:12 +0530

    s=input() k='' s=s.lower() c=0 for i in s:     if s.count(i)==1 and i!=' ':        print(i)        c=1        break if c==0:     print("-1")

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,10 +1,11 @@
 s=input()
-s.lower()
+k=''
+s=s.lower()
 c=0
 for i in s:
     if s.count(i)==1 and i!=' ':
-        print(i)
-        c=1
-        break
+       print(i)
+       c=1
+       break
 if c==0:
     print("-1")
