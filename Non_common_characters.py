58b3fe24c166828f88b29007d8acace466c4ed09
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-07-20 10:05:40 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() k='' for i in s1:     if i not in s2 and i not in k and i!=' ':         k+=i for i in s2:     if i not in s1 and i not in k and i!=' ':         k+=i print(len(k))

---
+++ Non_common_characters.py
@@ -0,0 +1,12 @@
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+k=''
+for i in s1:
+    if i not in s2 and i not in k and i!=' ':
+        k+=i
+for i in s2:
+    if i not in s1 and i not in k and i!=' ':
+        k+=i
+print(len(k))
